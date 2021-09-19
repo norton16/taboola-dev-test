3e71c045643b2a4d15deb0bd80d87a04466f8e27
Brian Norton <33581086+norton16@users.noreply.github.com>
2021-09-19 18:00:53 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-// Fetch webpage text and common words data
 const pageText = document.body.innerText;
 const commonWordsUrl = 'https://gist.githubusercontent.com/Thessiah/fb969b429b4d6173916628c7d92bf6e4/raw/fb30bf33cbade43fd667c45437d4937b53ce868a/top1k.json';
 
-// PART 1 - 25 Most Common Words
+
+// PART 1 - 25 MOST COMMON WORDS
 const mostCommonWords = async (str) => {
-    // Regex has zero width assertions on either side of a alphabetic string of length of at least 2
-    // to ensure no punctiation or coding syntax in or around words
-    // [^\d\W] - no decimals or non-word characters allowed
+    // This regex has zero-width assertions on either side of a alphabetic string of length of at least 2 and ensures no punctuation or coding syntax in or around words
     const words = str.match(/\b([^\d\W]{2,})+\b/g);
 
-    // Fetch words list
-     // Filter with common words list
+    // Fetch words list and filter with common words list
     const response = await fetch(commonWordsUrl);
     const data = await response.json();
     const filteredWords = words.filter(word => !data.includes(word.toLowerCase()));
@@ -18,8 +15,8 @@ const mostCommonWords = async (str) => {
         return item.toLowerCase();
     });
 
-    // 25 most common words on page
-    let hash = {};
+    // Find 25 most common words on page
+    const hash = {};
 
     for (let item of lowerCasedWords) {
         if(!hash[item]) {
@@ -29,18 +26,26 @@ const mostCommonWords = async (str) => {
 
     const hashArray = Object.entries(hash);
     const sortedHashArray = hashArray.sort((a,b) => b[1] - a[1]);
-    //console.log(sortedHashArray.slice(0,25));
-    return sortedHashArray.slice(0,25);    
+    // print and return 25 most common words
+    const finalList = sortedHashArray.slice(0,25);
+    console.log(finalList);
+    return finalList;    
 }
 
-// PART 2 - Replace Common Words
+
+// PART 2 - REPLACE COMMON WORDS
 const replaceCommonText = async (str) => {
     const wordsList = await mostCommonWords(str);
     for (let item of wordsList) {
         const regex = new RegExp('\\b(' + item[0] + ')+\\b', 'gi');
+        str = str.replaceAll(regex, item[1].toString());
         document.body.innerHTML = document.body.innerHTML.replaceAll(regex, item[1].toString());
     }
+    // print updated string
+    console.log(str);
 }
 
-//mostCommonWords(pageText)
-replaceCommonText(pageText);
+
+// RUN - run parts 1 & 2 respectively with argument pageText or any string:
+// mostCommonWords();
+// replaceCommonText();
